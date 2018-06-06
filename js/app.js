79cab406068e634b9f1d9689ae1db5b4b5eb1cf7
Marie Johnson <38162878+johnsonkm91@users.noreply.github.com>
2018-06-05 22:40:58 -0700

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// if( lived === 'no') {
+//   alert('That is correct!');
+//  } else (lived === yes) 
+//    alert('Incorrect! ');
+
+alert('Welcome to Maries guessing game! To play, simply read the following statements and decide whether or not it is true. Yes = True  No = False');
+  
+var pet = prompt('I am a proud owner of a pet snake');
+  console.log('You chose: ' + pet);
+  var pet = confirm('You chose: ' + pet);
+
+  var city = prompt('I was born and raised in Seattle, WA.');
+  console.log('You chose: ' + city);
+  var city = confirm('You chose: ' + city);
+
+  var sibling = prompt('I have 3 sisters.');
+  console.log('You chose: ' + sibling);
+  var sibling = confirm('You chose: ' + sibling );
+
+  var jump = prompt('I bungee jumped off the Stratosphere hotel in Las Vegas 3 times.');
+  console.log('You chose: ' + jump);
+  var jump = confirm('You chose: ' + jump );
+
+  var lived = prompt('I lived in New York for 4 years.');
+  console.log('You chose: ' + lived);
+  var lived = confirm('You chose: ' + lived );
+  
+
+      
